f26d9e72e4bb2b2a37a956a2943d280d76114bcc
Jess <jess.tasstic@gmail.com>
2020-10-23 14:47:13 -0400

    Update script

--- scripts.js
+++ scripts.js
@@ -5,13 +5,36 @@
 */
 
 // Query selectors
-
-
+var nameInput = document.querySelector('#name');
+var emailInput = document.querySelector('#email');
+var addContactButton = document.querySelector('#register-btn');
+var displayContactButton = document.querySelector('#display-btn');
+var displayArea = document.querySelector('.display-area');
+var contacts = [];
 
 // Event listeners
-
-
+addContactButton.addEventListener('click', addContact);
+displayContactButton.addEventListener('click', displayContact);
 
 
 // Functions
+function addContact() {
+  localStorage.setItem("name", nameInput.value);
+  localStorage.setItem("email", emailInput.value);
+  nameInput.value = "";
+  emailInput.value = "";
+  addContacts();
+}
 
+function displayContact() {
+  contacts.forEach(con => {
+    displayArea.insertAdjacentHTML('beforeend', con);
+  });
+}
+
+function addContacts() {
+  var name = localStorage.getItem("name");
+  var email = localStorage.getItem("email");
+  var contact = `<p><b>Name: </b>` + name + `<br><b>Email: </b>` + email + `</p>`;
+  contacts.push(contact);
+}
